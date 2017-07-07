9f3768cb6fb5c7849c7e854abdc7b9aca4399253
ngonimapfumo <ngonimapfumo@hotmail.com>
2017-07-07 11:47:23 +0200

    Hourly updates

---
+++ app/src/main/java/ngonim/xyz/yourweather/Hour.java
@@ -0,0 +1,56 @@
+package ngonim.xyz.yourweather;
+
+/**
+ * Created by ngoni on 7/7/17.
+ */
+
+public class Hour {
+
+    private double mTemperature;
+    private String mSummary;
+    private long mTime;
+    private String mIcon;
+    private String mTimezone;
+
+    public Hour(){}
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String timezone) {
+        mTimezone = timezone;
+    }
+}
