843b99b9591b62aab7e1b337e8b17325be865730
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2020-07-29 08:57:56 +0200

    *permissions fix

--- app/src/main/java/ngonim/xyz/yourweather/ui/MainActivity.java
+++ app/src/main/java/ngonim/xyz/yourweather/ui/MainActivity.java
@@ -183,7 +183,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         switch (requestCode){
             case 123:
                 if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
-
+                    getForecast();
                 }
                 else {
                     finish();
